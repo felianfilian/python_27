a7691965be6a7a7c9eace4a202da4177d233971f
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-16 01:21:16 +0100

    unlimited arguments

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import tkinter
-window = tkinter.Tk()
-window.title("First GUI Project")
-window.minsize(width=800, height=600)
+import tkinter_test
+import arguments_test
 
-my_label = tkinter.Label(text="MyLabel", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+# tkinter_test.start()
+arguments_test.start()
 
-
-window.mainloop()
 
---
+++ tkinter_test.py
@@ -0,0 +1,13 @@
+
+def start():
+    import tkinter
+    window = tkinter.Tk()
+    window.title("First GUI Project")
+    window.minsize(width=800, height=600)
+
+    my_label = tkinter.Label(text="MyLabel", font=("Arial", 24, "bold"))
+    my_label.pack(side="left")
+
+
+    window.mainloop()
+
