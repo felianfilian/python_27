7ccb8aa5534389fee425c63df1243e661536b3be
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-16 01:12:52 +0100

    label testing

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import tkinter
 window = tkinter.Tk()
+window.title("First GUI Project")
+window.minsize(width=800, height=600)
+
+my_label = tkinter.Label(text="MyLabel", font=("Arial", 24, "bold"))
+my_label.pack(side="left")
+
 
 window.mainloop()
+
