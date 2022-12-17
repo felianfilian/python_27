e22a117ed144ba3d7acf2419b632aad97497497d
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-17 13:26:49 +0100

    miles to kilometers project init

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tkinter_test
 import arguments_test
+import mile_kilometer_calc
 
-tkinter_test.start()
+# tkinter_test.start()
 # arguments_test.start()
-
+mile_kilometer_calc.
 
---
+++ mile_kilometer_calc.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+def start():
+    window = Tk()
+    window.title("Miles to Kilometers")
+    window.minsize(width=300, height=300)
+
+
+    window.mainloop()
