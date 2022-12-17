4072197d457e59f9b2cfde619a094eccf5c25df0
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-17 01:25:16 +0100

    grid layout

--- tkinter_test.py
+++ tkinter_test.py
@@ -5,19 +5,24 @@ def start():
     window.title("First GUI Project")
     window.minsize(width=800, height=600)
 
-    my_label = Label(text="MyLabel", font=("Arial", 24, "bold"))
-    my_label.pack(side="left")
-
     def button_click():
         my_label.config(text=f"{input.get()}")
 
+    my_label = Label(text="MyLabel", font=("Arial", 24, "bold"))
+    my_label.grid(column=0, row=0)
+
     # Button
     button = Button(text="Click", command=button_click)
-    button.pack()
+    button.grid(column=1, row=1)
+
+    button2 = Button(text="Click", command=button_click)
+    button2.grid(column=3, row=0)
 
     # Entry
     input = Entry(width=10)
-    input.pack()
+    input.grid(column=4, row=3)
+
+
 
     window.mainloop()
 
