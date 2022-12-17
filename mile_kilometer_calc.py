f06567ef5aa12392688195135e4089de2c01e902
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-17 19:40:40 +0100

    miles km calculatior finished

--- mile_kilometer_calc.py
+++ mile_kilometer_calc.py
@@ -6,6 +6,10 @@ def start():
     window.minsize(width=300, height=300)
     window.config(padx=50, pady=50)
 
+    def calc_km():
+        result = float(entry.get()) * 1.61
+        label03.config(text=str(result))
+
     entry = Entry(width=10)
     entry.grid(column=1, row=0)
 
@@ -21,7 +25,7 @@ def start():
     label04 = Label(text="Km")
     label04.grid(column=2, row=1)
 
-    btn_calc = Button(text="Calculate")
+    btn_calc = Button(text="Calculate", command=calc_km)
     btn_calc.grid(column=1, row=2)
 
     window.mainloop()
