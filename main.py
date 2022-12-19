63d47dc7acc4d059fa4710b43e8f12ca0c24b803
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-19 09:54:49 +0100

    password manager setup

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import tkinter_test
 import arguments_test
 import mile_kilometer_calc
 import pomodoro_timer
+import password_manager
 
 # tkinter_test.start()
 # arguments_test.start()
 # mile_kilometer_calc.start()
-pomodoro_timer.start()
-
+# pomodoro_timer.start()
+password_manager.start()
---
+++ password_manager.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+def start():
+    window = Tk()
+    window.title("Password Manager")
+    canvas = Canvas(height=200, width=200)
+    canvas.pack()
+
+    window.mainloop()
+
