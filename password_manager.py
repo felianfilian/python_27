cc336edc115459c10fae9ae551cb2dfd9f8014aa
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-19 10:11:55 +0100

    gui screen setup

--- password_manager.py
+++ password_manager.py
@@ -4,6 +4,8 @@ def start():
     window = Tk()
     window.title("Password Manager")
     canvas = Canvas(height=200, width=200)
+    logo_img = PhotoImage(file="./pw_manager/logo.png")
+    canvas.create_image(100, 100, image=logo_img)
     canvas.pack()
 
     window.mainloop()
