f577aded7d05c05c2b1817ad937e342f48d88840
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-21 12:47:19 +0100

    KeyError test

--- exceptions.py
+++ exceptions.py
@@ -1,10 +1,13 @@
 
 
 def start():
-    try:
-        fruit = ["apple"]
-        print(fruit[1])
-    except:
-        print("no index")
+    total = 0
+    names = [{"name": "luigi", "likes": 12,}, { "name": "mario"}, { "name": "peach", "likes": 12,}]
+    for name in names:
+        try:
+            total = total + name["likes"]  # gives an error for mario
+        except KeyError:
+            pass
+    print(total)
 
-    
+
